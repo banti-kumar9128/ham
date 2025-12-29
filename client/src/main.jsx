95b857e1eacc976c4a21dff5b064fd11deae18e4
banti-kumar9128 <vk9128697037@gmail.com>
2025-12-29 16:38:03 +0530

    Add SPA routing support for GitHub Pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,8 +4,14 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { AppContextProvider } from "./context/AppContext.jsx";
+
+// Handle GitHub Pages SPA routing
+if (window.location.pathname.startsWith("/?")) {
+  window.history.replaceState(null, "", window.location.pathname.slice(1));
+}
+
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/ham">
     <AppContextProvider>
       <App />
     </AppContextProvider>
